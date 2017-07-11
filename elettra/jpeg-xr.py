ad6b2c1cd444443ddfc0a10c9ad9842ffcd76d4e
Francesco De Carlo <decarlof@gmail.com>
2017-07-11 18:30:18 -0500

    first commit

---
+++ elettra/jpeg-xr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Python example of how to read am images.
+"""
+
+from __future__ import print_function
+import numpy as np
+import imageio
+import sys
+
+  
+def main(arg):
+
+    top = '/Users/decarlo/Desktop/util/elettra/img/'
+    print(imageio.help('JPEG-XR-FI'))
+    # im = imageio.imread(top + "SMALLTOMATO.jxr")
+    # imageio.imsave(top + "test.jxr", im)
+    
+    im = imageio.imread(top + "lena.tif")
+    imageio.imsave(top + "test_00.jxr", im)
+    imageio.imsave(top + "test_01.tiff", im)
+    imageio.imsave(top + "test_02.png", im)
+    imageio.imsave(top + "test_03.jpeg", im, X=100)
+    imageio.imsave(top + "test_04.jpeg", im, flag='JPEG_QUALITYBAD')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
