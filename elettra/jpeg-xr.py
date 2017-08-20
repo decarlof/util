d884240276e0b0a222a5527669adc02d521b81a3
Francesco De Carlo <decarlof@gmail.com>
2017-08-20 00:04:59 -0500

    update

--- elettra/jpeg-xr.py
+++ elettra/jpeg-xr.py
@@ -10,7 +10,9 @@ import numpy as np
 import imageio
 import sys
 
-  
+# see http://programtalk.com/python-examples/imageio.imsave/
+# for exmaples
+
 def main(arg):
 
     top = '/Users/decarlo/Desktop/util/elettra/img/'
@@ -20,10 +22,10 @@ def main(arg):
     # imageio.imsave(top + "test.jxr", im)
     
     im = imageio.imread(top + "lena.tif")
-    imageio.imsave(top + "test_00.jxr", im)
+    #imageio.imsave(top + "test_00.jxr", im)
     imageio.imsave(top + "test_01.tiff", im)
     imageio.imsave(top + "test_02.png", im)
-    imageio.imsave(top + "test_03.jpeg", im, X=100)
+    print(imageio.imsave(top + "test_03.jpeg", im, progressive=True, optimize=True, baseline=True))
     imageio.imsave(top + "test_04.jpeg", im, flag='JPEG_QUALITYBAD')
 
 if __name__ == "__main__":
