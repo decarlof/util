7933115e50632b7d2af62971ee5213e894e3c7f4
Francesco De Carlo <decarlof@gmail.com>
2018-05-21 10:47:32 -0500

    first commit

---
+++ cnn/transform_predict_vda.py
@@ -0,0 +1,57 @@
+import os
+import re
+import dxchange
+import time
+from xlearn.transform import model
+from xlearn.transform import predict
+
+
+batch_size = 800
+nb_epoch = 40
+dim_img = 20
+nb_filters = 32
+nb_conv = 3
+patch_step = 4
+
+patch_size = (dim_img, dim_img)
+
+mdl = model(dim_img, nb_filters, nb_conv)
+mdl.load_weights('transform_training_weights_vda.h5')
+
+print('Predicting')
+
+folder = '/local/dataraid/'
+files = [f for f in sorted(os.listdir(folder)) if re.match(r'.+.tif', f)]
+
+for fname in files:
+    time_start = time.time()
+    sname = fname.split('.')
+    time_start = time.time()
+    fname_save = folder + sname[0] + '_result'
+    img_test = dxchange.read_tiff(folder + fname)
+    img_rec = predict(mdl, img_test, patch_size, patch_step, batch_size, dim_img)
+    dxchange.write_tiff(img_rec, fname_save, dtype='float32')
+    print(time.time()-time_start)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+       
---
+++ cnn/transform_train_vda.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Example script 
+"""
+
+from __future__ import print_function
+from xlearn.transform import train
+import dxchange
+
+
+batch_size = 800
+nb_epoch = 10
+dim_img = 20
+nb_filters = 32
+nb_conv = 3
+patch_step = 4
+
+patch_size = (dim_img, dim_img)
+
+# read the training data
+
+img_x = dxchange.read_tiff('data/training_input_vda.tiff')
+img_y = dxchange.read_tiff('data/training_output_vda.tiff')
+
+# train and save the model
+model = train(img_x, img_y, patch_size, patch_step, dim_img, nb_filters, nb_conv, batch_size, nb_epoch)
+model.save_weights('transform_training_weights_vda.h5')
+
