6640ce1495da546a24d3002ba5601b54f65fee7d
Francesco De Carlo <deccarlof@gmail.com>
2018-11-20 21:45:38 -0600

    update

--- ken/rec.sh
+++ ken/rec.sh
@@ -1,3 +1,9 @@
-#python rec.py --axis 633.5 --type full /local/data/2018-11/Kemner/T10_22micro_0001.h5
-#python rec.py --axis 623 --type full  /local/data/2018-11/Kemner/T5_36micro_0002.h5
+python rec.py --axis 633.5 --type full /local/data/2018-11/Kemner/T10_22micro_0001.h5
+python rec.py --axis 623 --type full  /local/data/2018-11/Kemner/T5_36micro_0002.h5
+python rec.py --axis 554  --type full --method sirtfbp /local/data/2018-11/Kemner/T15_31micro_0001.h5
 python rec.py --axis 1292 --type full /local/data/2018-11/Kemner/T15_38macro.h5
+python rec.py --axis 620 --type full /local/data/2018-11/Kemner/T30_33micro_0001.h5
+python rec.py --axis 620 --type full /local/data/2018-11/Kemner/T5_28macro_0002.h5 
+python rec.py --axis 554 --type full /local/data/2018-11/Kemner/T30_24macro_0001.h5
+python rec.py --axis 564 --type full /local/data/2018-11/Kemner/control_noQD_0001.h5
+python rec.py --axis 1299.2 --type full /local/data/2018-11/Kemner/T10_31macro_0001.h5
