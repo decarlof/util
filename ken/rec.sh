2db828c5095a3546ce46223ddc09daf508a593cd
Francesco De Carlo <deccarlof@gmail.com>
2018-11-20 18:59:33 -0600

    First commit

---
+++ ken/rec.sh
@@ -0,0 +1,3 @@
+#python rec.py --axis 633.5 --type full /local/data/2018-11/Kemner/T10_22micro_0001.h5
+#python rec.py --axis 623 --type full  /local/data/2018-11/Kemner/T5_36micro_0002.h5
+python rec.py --axis 1292 --type full /local/data/2018-11/Kemner/T15_38macro.h5
