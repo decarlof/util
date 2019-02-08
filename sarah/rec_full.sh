7b389b0423a4c9665ff5a2a52399c7e39facbc4d
Francesco De Carlo <deccarlof@gmail.com>
2019-02-08 15:36:05 -0600

    first commit

---
+++ sarah/rec_full.sh
@@ -0,0 +1,6 @@
+python rec.py --type slice --method gridrec /local/data/2019-02/Wolff/4140S21exp1_0002.h5
+python rec.py --type slice --method gridrec /local/data/2019-02/Wolff/4140S21exp1_10X_0004.h5
+python rec.py --type slice --method gridrec /local/data/2019-02/Wolff/4140S21exp2_1_10X_0005.h5
+python rec.py --type slice --method gridrec /local/data/2019-02/Wolff/4140S4exp23_1_10X_0006.h5
+python rec.py --type slice --method gridrec /local/data/2019-02/Wolff/4140S4exp23_2_10X_0007.h5
+
