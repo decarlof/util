c7d44d8dbfe8cdf08ccd3b2f0b0301be25ad8d1d
Francesco De Carlo <deccarlof@gmail.com>
2019-02-08 18:54:21 -0600

    update

--- sarah/rec_full.sh
+++ sarah/rec_full.sh
@@ -1,6 +1,6 @@
-python rec.py --type slice --method gridrec /local/data/2019-02/Wolff/4140S21exp1_0002.h5
-python rec.py --type slice --method gridrec /local/data/2019-02/Wolff/4140S21exp1_10X_0004.h5
-python rec.py --type slice --method gridrec /local/data/2019-02/Wolff/4140S21exp2_1_10X_0005.h5
-python rec.py --type slice --method gridrec /local/data/2019-02/Wolff/4140S4exp23_1_10X_0006.h5
-python rec.py --type slice --method gridrec /local/data/2019-02/Wolff/4140S4exp23_2_10X_0007.h5
-
+python rec.py --axis 1309.6 --type full /local/data/2019-02/Wolff/4140S4exp21_1_10X_0008.h5
+python rec.py --axis 1316.5 --type full /local/data/2019-02/Wolff/4140S4exp21_2_10X_0009.h5
+python rec.py --axis 1337 --type full  /local/data/2019-02/Wolff/SS304L_Printed_Sample_1_0010.h5
+python rec.py --axis 1320.8 --type full /local/data/2019-02/Wolff/Ti64_S6_exp34_1_0012.h5
+python rec.py --axis 1319 --type full /local/data/2019-02/Wolff/Ti64_S6_exp34_2_0013.h5
+python rec.py --axis 1318 --type full /local/data/2019-02/Wolff/Ti64_S6_exp33_1_0014.h5
