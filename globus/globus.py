4a96ff56b32eddb041a056ff6438fa72a413ba26
Francesco De Carlo <decarlof@gmail.com>
2019-05-21 23:13:09 -0500

    update

--- globus/globus.py
+++ globus/globus.py
@@ -1,43 +1,27 @@
-import globus_sdk
-import time
-from globus_lib import *
+import globus_lib
 
-CLIENT_ID = "8235a963-59a6-4354-9724-d330025b199d"
+__author__ = "Francesco De Carlo"
+__copyright__ = "Copyright (c) 2019, UChicago Argonne, LLC."
+__version__ = "0.0.1"
+__docformat__ = 'restructuredtext en'
 
-client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
-client.oauth2_start_flow(refresh_tokens=True)
+# see https://globus-sdk-python.readthedocs.io/en/stable/tutorial/#step-1-get-a-client
+# to create your project app_id
+app_id = "8235a963-59a6-4354-9724-d330025b199d"
 
-print('Please go to this URL and login: {0}'.format(client.oauth2_get_authorize_url()))
-
-get_input = getattr(__builtins__, 'raw_input', input)
-auth_code = get_input(
-    'Please enter the code you get after login here: ').strip()
-token_response = client.oauth2_exchange_code_for_tokens(auth_code)
-
-# let's get stuff for the Globus Transfer service
-globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
-# the refresh token and access token, often abbr. as RT and AT
-transfer_rt = globus_transfer_data['refresh_token']
-transfer_at = globus_transfer_data['access_token']
-expires_at_s = globus_transfer_data['expires_at_seconds']
-
-# Now we've got the data we need, but what do we do?
-# That "GlobusAuthorizer" from before is about to come to the rescue
-authorizer = globus_sdk.RefreshTokenAuthorizer(transfer_rt, client, access_token=transfer_at, expires_at=expires_at_s)
-# and try using `tc` to make TransferClient calls. Everything should just
-# work -- for days and days, months and months, even years
-tc = globus_sdk.TransferClient(authorizer=authorizer)
-ac = globus_sdk.AuthClient(authorizer=authorizer)
-# high level interface; provides iterators for list responses
-# while(not time.sleep(5)):
-#     print("My Endpoints:")
-#     for ep in tc.endpoint_search(filter_scope="my-endpoints"):
-#         print("[{}] {}".format(ep["id"], ep["display_name"]))
+ac, tc = globus_lib.create_clients(app_id)
 
 print("Endpoints shared with me:")
 for ep in tc.endpoint_search(filter_scope="shared-with-me"):
     print("[{}] {}".format(ep["id"], ep["display_name"]))
 
+# print output for the endpoint shared with me:
+# [ad484910-0842-11e7-bb15-22000b9a448b] aps_32id
+# [26a93324-0847-11e7-bb15-22000b9a448b] nersc_aps_32id
+# [e133a81a-6d04-11e5-ba46-22000b92c6ec] petrel tomography
+
+# picked petrel
 petrel_id = u'e133a81a-6d04-11e5-ba46-22000b92c6ec'
 
-rdp(ac, tc, petrel_id, '/2-BM/', '2020-06', 'decarlof@gmail.com')
+shared_path = globus_lib.create_dir('2020-12', petrel_id, '/2-BM/', ac, tc)
+globus_lib.share_dir(shared_path, 'decarlof@gmail.com', petrel_id, ac, tc)
--- globus/globus_lib.py
+++ globus/globus_lib.py
@@ -1,21 +1,78 @@
-import sys, random
+import globus_sdk
 
-def rdp(ac,           # Authorize client
-        tc,           # Transfer client
-        endpoint_id,  # Endpoint id on which to create shared folder
-        endpoint_top, # Endpoint top directory
-        new_share,    # Subdirectory name under top to be created and shared with user
-        email):       # User email address for notification
+__author__ = "Francesco De Carlo"
+__copyright__ = "Copyright (c) 2019, UChicago Argonne, LLC."
+__version__ = "0.0.1"
+__docformat__ = 'restructuredtext en'
+
+__all__ = ['create_clients',
+           'create_dir',
+           'share_dir']
+
+def create_clients(app_id):
+  """
+  Create authorize and transfer clients
+
+  Parameters
+  ----------
+  app_id : App UUID 
+
+  Returns
+  -------
+  ac : Authorize client
+  tc : Transfer client
+    
+    """
+
+  client = globus_sdk.NativeAppAuthClient(app_id)
+  client.oauth2_start_flow(refresh_tokens=True)
+
+  print('Please go to this URL and login: {0}'.format(client.oauth2_get_authorize_url()))
+
+  get_input = getattr(__builtins__, 'raw_input', input)
+  auth_code = get_input(
+      'Please enter the code you get after login here: ').strip()
+  token_response = client.oauth2_exchange_code_for_tokens(auth_code)
+
+  # let's get stuff for the Globus Transfer service
+  globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
+  # the refresh token and access token, often abbr. as RT and AT
+  transfer_rt = globus_transfer_data['refresh_token']
+  transfer_at = globus_transfer_data['access_token']
+  expires_at_s = globus_transfer_data['expires_at_seconds']
+
+  # Now we've got the data we need, but what do we do?
+  # That "GlobusAuthorizer" from before is about to come to the rescue
+  authorizer = globus_sdk.RefreshTokenAuthorizer(transfer_rt, client, access_token=transfer_at, expires_at=expires_at_s)
+  # and try using `tc` to make TransferClient calls. Everything should just
+  # work -- for days and days, months and months, even years
+  ac = globus_sdk.AuthClient(authorizer=authorizer)
+  tc = globus_sdk.TransferClient(authorizer=authorizer)
+
+  return ac, tc
+
+def create_dir(new_share,     # Subdirectory name under top to be created and shared with user
+               endpoint_id,   # Endpoint id on which to create shared folder
+               endpoint_top,  # Endpoint top directory
+               ac,            # Authorize client  
+               tc):           # Transfer client
 
     # Create directory to be shared
     share_path = endpoint_top + new_share + '/'
     tc.operation_mkdir(endpoint_id, path=share_path)
+    return share_path
+
+def share_dir(share_path,     # Endpoint path to shared folder
+              email,          # User email address for notification
+              endpoint_id,    # Endpoint id on which to create shared folder
+              ac,             # Authorize client  
+              tc):            # Transfer client
 
     # Generate user id from user email
     r = ac.get_identities(usernames=email)
     user_id = r['identities'][0]['id']
     # print(r, user_id)
-    
+
     # Set access control and notify user
     rule_data = {
       'DATA_TYPE': 'access',
