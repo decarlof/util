da6fd55792ceb763c377928b8b29439abd25f6e1
Francesco De Carlo <deccarlof@gmail.com>
2019-10-23 11:59:46 -0500

    McDowell 2019-10

---
+++ McDowell/tomosaic.sh
@@ -0,0 +1,281 @@
+mkdir 048_1mA_cm2_LSPS_longplating
+mkdir 046_1mA_cm2_LSPS_longplating
+mkdir 044_1mA_cm2_LSPS_longplating
+mkdir 042_1mA_cm2_LSPS_longplating
+mkdir 040_1mA_cm2_LSPS_longplating
+mkdir 038_1mA_cm2_LSPS_longplating
+mkdir 036_1mA_cm2_LSPS_longplating
+mkdir 034_1mA_cm2_LSPS_longplating
+mkdir 032_1mA_cm2_LSPS_longplating
+mkdir 030_1mA_cm2_LSPS_longplating
+mkdir 028_1mA_cm2_LSPS_longplating
+mkdir 026_1mA_cm2_LSPS_longplating
+mkdir 024_1mA_cm2_LSPS_longplating
+mkdir 022_1mA_cm2_LSPS_longplating
+mkdir 020_1mA_cm2_LSPS_longplating
+mkdir 018_1mA_cm2_LSPS_longplating
+mkdir 016_1mA_cm2_LSPS_longplating
+mkdir 014_1mA_cm2_LSPS_longplating
+mkdir 012_1mA_cm2_LSPS_longplating
+mkdir 010_1mA_cm2_LSPS_longplating
+mv 048*.h5 048_1mA_cm2_LSPS_longplating/
+mv 046*.h5 046_1mA_cm2_LSPS_longplating/
+mv 044*.h5 044_1mA_cm2_LSPS_longplating/
+mv 042*.h5 042_1mA_cm2_LSPS_longplating/
+mv 040*.h5 040_1mA_cm2_LSPS_longplating/
+mv 038*.h5 038_1mA_cm2_LSPS_longplating/
+mv 036*.h5 036_1mA_cm2_LSPS_longplating/
+mv 034*.h5 034_1mA_cm2_LSPS_longplating/
+mv 032*.h5 032_1mA_cm2_LSPS_longplating/
+mv 030*.h5 030_1mA_cm2_LSPS_longplating/
+mv 028*.h5 028_1mA_cm2_LSPS_longplating/
+mv 026*.h5 026_1mA_cm2_LSPS_longplating/
+mv 024*.h5 024_1mA_cm2_LSPS_longplating/
+mv 022*.h5 022_1mA_cm2_LSPS_longplating/
+mv 020*.h5 020_1mA_cm2_LSPS_longplating/
+mv 018*.h5 018_1mA_cm2_LSPS_longplating/
+mv 016*.h5 016_1mA_cm2_LSPS_longplating/
+mv 014*.h5 014_1mA_cm2_LSPS_longplating/
+mv 012*.h5 012_1mA_cm2_LSPS_longplating/
+mv 010*.h5 010_1mA_cm2_LSPS_longplating/
+mv 051*.h5 050_1mA_cm2_LSPS_longplating/
+mv 049*.h5 048_1mA_cm2_LSPS_longplating/
+mv 047*.h5 046_1mA_cm2_LSPS_longplating/
+mv 045*.h5 044_1mA_cm2_LSPS_longplating/
+mv 043*.h5 042_1mA_cm2_LSPS_longplating/
+mv 041*.h5 040_1mA_cm2_LSPS_longplating/
+mv 039*.h5 038_1mA_cm2_LSPS_longplating/
+mv 037*.h5 036_1mA_cm2_LSPS_longplating/
+mv 035*.h5 034_1mA_cm2_LSPS_longplating/
+mv 033*.h5 032_1mA_cm2_LSPS_longplating/
+mv 031*.h5 030_1mA_cm2_LSPS_longplating/
+mv 029*.h5 028_1mA_cm2_LSPS_longplating/
+mv 027*.h5 026_1mA_cm2_LSPS_longplating/
+mv 025*.h5 024_1mA_cm2_LSPS_longplating/
+mv 023*.h5 022_1mA_cm2_LSPS_longplating/
+mv 021*.h5 020_1mA_cm2_LSPS_longplating/
+mv 019*.h5 018_1mA_cm2_LSPS_longplating/
+mv 017*.h5 016_1mA_cm2_LSPS_longplating/
+mv 015*.h5 014_1mA_cm2_LSPS_longplating/
+mv 013*.h5 012_1mA_cm2_LSPS_longplating/
+mv 011*.h5 010_1mA_cm2_LSPS_longplating/
+
+cp 002_4mA_cm2_LSPS_before/*.py 008_4mA_cm2_LSPS_before/
+cp 002_4mA_cm2_LSPS_before/*.py 010_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 012_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 014_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 016_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 018_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 020_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 022_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 024_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 026_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 028_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 030_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 032_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 034_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 036_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 038_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 040_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 042_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 044_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 046_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/*.py 048_1mA_cm2_LSPS_longplating/
+
+cd 008_4mA_cm2_LSPS_before/
+python mosaic_reorganize.py
+cd ../010_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../012_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../014_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../016_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../018_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../020_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../022_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../024_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../026_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../028_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../030_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../032_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../034_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../036_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../038_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../040_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../042_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../044_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../046_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+cd ../048_1mA_cm2_LSPS_longplating/
+python mosaic_reorganize.py
+
+cd ../008_4mA_cm2_LSPS_before/
+python mosaic_preview.py --pano 0
+cd ../010_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../012_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../014_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../016_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../018_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../020_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../022_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../024_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../026_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../028_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../030_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../032_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../034_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../036_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../038_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../040_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../042_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../044_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../046_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+cd ../048_1mA_cm2_LSPS_longplating/
+python mosaic_preview.py --pano 0
+
+cd ../008_4mA_cm2_LSPS_before/
+python mosaic_find_center.py
+cd ../010_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../012_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../014_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../016_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../018_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../020_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../022_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../024_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../026_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../028_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../030_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../032_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../034_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../036_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../038_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../040_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../042_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../044_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../046_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+cd ../048_1mA_cm2_LSPS_longplating/
+python mosaic_find_center.py
+
+# edit 002_4mA_cm2_LSPS_before/mosaic_recon.py
+cd ..
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 008_4mA_cm2_LSPS_before/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 010_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 012_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 014_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 016_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 018_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 020_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 022_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 024_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 026_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 028_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 030_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 032_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 034_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 036_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 038_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 040_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 042_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 044_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 046_1mA_cm2_LSPS_longplating/
+cp 002_4mA_cm2_LSPS_before/mosaic_recon.py 048_1mA_cm2_LSPS_longplating/
+
+cd 008_4mA_cm2_LSPS_before/
+python mosaic_recon.py
+cd ../010_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../012_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../014_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../016_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../018_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../020_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../022_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../024_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../026_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../028_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../030_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../032_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../034_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../036_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../038_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../040_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../042_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../044_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../046_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+cd ../048_1mA_cm2_LSPS_longplating/
+python mosaic_recon.py
+
+
