073aeb3b2d0b01d0dcdbbb687f53661d1fb647f3
Francesco De Carlo <deccarlof@gmail.com>
2019-10-23 11:22:33 -0500

    McDowell 2019-10

---
+++ McDowell/rename.sh
@@ -0,0 +1,61 @@
+mkdir 048_1mA_cm2_LSPS_longplating
+mkdir 046_1mA_cm2_LSPS_longplating
+mkdir 044_1mA_cm2_LSPS_longplating
+mkdir 042_1mA_cm2_LSPS_longplating
+mkdir 040_1mA_cm2_LSPS_longplating
+mkdir 038_1mA_cm2_LSPS_longplating
+mkdir 036_1mA_cm2_LSPS_longplating
+mkdir 034_1mA_cm2_LSPS_longplating
+mkdir 032_1mA_cm2_LSPS_longplating
+mkdir 030_1mA_cm2_LSPS_longplating
+mkdir 028_1mA_cm2_LSPS_longplating
+mkdir 026_1mA_cm2_LSPS_longplating
+mkdir 024_1mA_cm2_LSPS_longplating
+mkdir 022_1mA_cm2_LSPS_longplating
+mkdir 020_1mA_cm2_LSPS_longplating
+mkdir 018_1mA_cm2_LSPS_longplating
+mkdir 016_1mA_cm2_LSPS_longplating
+mkdir 014_1mA_cm2_LSPS_longplating
+mkdir 012_1mA_cm2_LSPS_longplating
+mkdir 010_1mA_cm2_LSPS_longplating
+mv 048*.h5 048_1mA_cm2_LSPS_longplating/
+mv 046*.h5 046_1mA_cm2_LSPS_longplating/
+mv 044*.h5 044_1mA_cm2_LSPS_longplating/
+mv 042*.h5 042_1mA_cm2_LSPS_longplating/
+mv 040*.h5 040_1mA_cm2_LSPS_longplating/
+mv 038*.h5 038_1mA_cm2_LSPS_longplating/
+mv 036*.h5 036_1mA_cm2_LSPS_longplating/
+mv 034*.h5 034_1mA_cm2_LSPS_longplating/
+mv 032*.h5 032_1mA_cm2_LSPS_longplating/
+mv 030*.h5 030_1mA_cm2_LSPS_longplating/
+mv 028*.h5 028_1mA_cm2_LSPS_longplating/
+mv 026*.h5 026_1mA_cm2_LSPS_longplating/
+mv 024*.h5 024_1mA_cm2_LSPS_longplating/
+mv 022*.h5 022_1mA_cm2_LSPS_longplating/
+mv 020*.h5 020_1mA_cm2_LSPS_longplating/
+mv 018*.h5 018_1mA_cm2_LSPS_longplating/
+mv 016*.h5 016_1mA_cm2_LSPS_longplating/
+mv 014*.h5 014_1mA_cm2_LSPS_longplating/
+mv 012*.h5 012_1mA_cm2_LSPS_longplating/
+mv 010*.h5 010_1mA_cm2_LSPS_longplating/
+mv 051*.h5 050_1mA_cm2_LSPS_longplating/
+mv 049*.h5 048_1mA_cm2_LSPS_longplating/
+mv 047*.h5 046_1mA_cm2_LSPS_longplating/
+mv 045*.h5 044_1mA_cm2_LSPS_longplating/
+mv 043*.h5 042_1mA_cm2_LSPS_longplating/
+mv 041*.h5 040_1mA_cm2_LSPS_longplating/
+mv 039*.h5 038_1mA_cm2_LSPS_longplating/
+mv 037*.h5 036_1mA_cm2_LSPS_longplating/
+mv 035*.h5 034_1mA_cm2_LSPS_longplating/
+mv 033*.h5 032_1mA_cm2_LSPS_longplating/
+mv 031*.h5 030_1mA_cm2_LSPS_longplating/
+mv 029*.h5 028_1mA_cm2_LSPS_longplating/
+mv 027*.h5 026_1mA_cm2_LSPS_longplating/
+mv 025*.h5 024_1mA_cm2_LSPS_longplating/
+mv 023*.h5 022_1mA_cm2_LSPS_longplating/
+mv 021*.h5 020_1mA_cm2_LSPS_longplating/
+mv 019*.h5 018_1mA_cm2_LSPS_longplating/
+mv 017*.h5 016_1mA_cm2_LSPS_longplating/
+mv 015*.h5 014_1mA_cm2_LSPS_longplating/
+mv 013*.h5 012_1mA_cm2_LSPS_longplating/
+mv 011*.h5 010_1mA_cm2_LSPS_longplating/
