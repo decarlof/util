e4b0656cdd11b1843d87ec798d0439cbe5916b4e
Francesco De Carlo <decarlof@gmai.com>
2020-02-01 11:46:13 -0700

    first commit

--- zihengWu/convert.py
+++ zihengWu/convert.py
@@ -3,31 +3,43 @@
 import dxfile.dxtomo as dx
 import dxchange 
 
-projfname = '/loca/data/proj_201.hdf'
 
-head_tail = os.path.split(projfname)
 
-darkfname = head_tail[0] + os.sep + "proj_0203.hdf"
-whitefname = head_tail[0] + os.sep + "proj_0202.hdf"
-fixedfmame = head_tail[0] + os.sep + 'proj_201.h5'
+def main(args):
 
-exchange_base = "exchange"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fname", help="file name of a tomographic dataset: /data/ or /data/sample.hdf")
+    args = parser.parse_args()
 
-tomo_grp = '/'.join([exchange_base, 'data'])
-flat_grp = '/'.join([exchange_base, 'data_white'])
-dark_grp = '/'.join([exchange_base, 'data_dark'])
-theta_grp = '/'.join([exchange_base, 'theta'])
-tomo = dxreader.read_hdf5(projfname, tomo_grp)
-flat = dxreader.read_hdf5(whitefname, flat_grp)
-dark = dxreader.read_hdf5(darkfname, dark_grp)
-theta = dxreader.read_hdf5(projfname, theta_grp)
+    projfname = args.fname
 
-# Open DataExchange file
-f = dx.File(fixedfmame, mode='w') 
+    head_tail = os.path.split(projfname)
 
-f.add_entry(dx.Entry.data(data={'value': data, 'units':'counts'}))
-f.add_entry(dx.Entry.data(data_white={'value': data_white, 'units':'counts'}))
-f.add_entry(dx.Entry.data(data_dark={'value': data_dark, 'units':'counts'}))
-f.add_entry(dx.Entry.data(theta={'value': theta, 'units':'degrees'}))
+    darkfname = head_tail[0] + os.sep + "proj_0203.hdf"
+    whitefname = head_tail[0] + os.sep + "proj_0202.hdf"
+    fixedfmame = head_tail[0] + os.sep + 'proj_201.h5'
 
-f.close()
+    exchange_base = "exchange"
+
+    tomo_grp = '/'.join([exchange_base, 'data'])
+    flat_grp = '/'.join([exchange_base, 'data_white'])
+    dark_grp = '/'.join([exchange_base, 'data_dark'])
+    theta_grp = '/'.join([exchange_base, 'theta'])
+    tomo = dxreader.read_hdf5(projfname, tomo_grp)
+    flat = dxreader.read_hdf5(whitefname, flat_grp)
+    dark = dxreader.read_hdf5(darkfname, dark_grp)
+    theta = dxreader.read_hdf5(projfname, theta_grp)
+
+    # Open DataExchange file
+    f = dx.File(fixedfmame, mode='w') 
+
+    f.add_entry(dx.Entry.data(data={'value': data, 'units':'counts'}))
+    f.add_entry(dx.Entry.data(data_white={'value': data_white, 'units':'counts'}))
+    f.add_entry(dx.Entry.data(data_dark={'value': data_dark, 'units':'counts'}))
+    f.add_entry(dx.Entry.data(theta={'value': theta, 'units':'degrees'}))
+
+    f.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
