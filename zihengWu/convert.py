c3eac7059b68ec384c0db02d06ac0f3eed253c90
Francesco De Carlo <decarlof@gmai.com>
2020-02-01 12:45:17 -0700

    first commit

--- zihengWu/convert.py
+++ zihengWu/convert.py
@@ -21,6 +21,9 @@ def main(args):
     white_fname = head_tail[0] + os.sep + "proj_0202.hdf"
     converted_fmame = head_tail[0] + os.sep + os.path.splitext(head_tail[1])[1] + '.h5'
 
+    print(head_tail)
+    print(head_tail[0])
+    print(head_tail[1])
     print(proj_fname)
     print(dark_fname)
     print(white_fname)
