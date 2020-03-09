45d60d60c213daa19c9594b9cb23d7ab76f58b99
Francesco De Carlo <decarlof@gmail.com>
2020-03-09 12:25:13 -0500

    update 2020-03

--- sangid/logload.py
+++ sangid/logload.py
@@ -11,7 +11,8 @@ import numpy as np
 from epics import PV
 
 variableDict = {
-        'IOC_Prefix': 'PCOIOC3:', 
+        # 'IOC_Prefix': 'PCOIOC3:', 
+        'IOC_Prefix': '2bmbSP1:', 
         'Station': '2-BM-A'
         }
 
@@ -41,7 +42,7 @@ def main():
                         global_PVs['LoadNewton'].get(), global_PVs['LoadVoltage'].get(), h5fname_str))
                 print('%s %s; Load: %4.4f N (%4.4f V)' % (h5fname_str, datetime.datetime.now().isoformat(), \
                         global_PVs['LoadNewton'].get(), global_PVs['LoadVoltage'].get()))
-                time.sleep(2)	
+                time.sleep(.25)	
     except KeyboardInterrupt:
 	
         print('interrupted!')
